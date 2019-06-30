0d3458a393221988a9fee8090decb40a8739c4df
root <root@vost-blog.c.admob-app-id-6663345165.internal>
2019-06-30 13:38:33 +0000

    fix(Mailchimp): Reworked files to prevent publications of API Keys;

--- wp-content/themes/vost/subscribe.php
+++ wp-content/themes/vost/subscribe.php
@@ -1,4 +1,5 @@
 <?php
+
 $data = [
     'email' => "",
     'status' => 'subscribed',
@@ -6,7 +7,9 @@ $data = [
 $data["email"] = $_POST["email"];
 function syncMailchimp($data)
 {
-    $apiKey = 'c9af557764c5f9ce4c0f846c1203b5d1-us20';
+    include('subscribe.keys.php');
+    $apiKey = $MAILCHIMP_API_KEY;
+    $listId = $MAILCHIMP_LIST_ID;
     $listId = '741b62cca2';
     $memberId = md5(strtolower($data['email']));
     $dataCenter = substr($apiKey, strpos($apiKey, '-') + 1);
